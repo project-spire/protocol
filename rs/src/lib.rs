1224f6e5e279a965bc5bd6b8ffcf73b549d8e28c
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-03-18 15:31:56 +0900

    Add serialze util

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -12,6 +12,8 @@ pub mod net {
     include!(concat!(env!("OUT_DIR"), "/spire.protocol.net.rs"));
 }
 
+use prost::EncodeError;
+
 pub const HEADER_SIZE: usize = 4;
 
 #[derive(Eq, PartialEq, Debug)]
@@ -25,25 +27,35 @@ pub enum ProtocolCategory {
 #[derive(Debug, Clone)]
 pub enum SerializeError {
     BodyLengthExceeded,
+    EncodeError(EncodeError),
+}
+
+pub fn serialize<T: prost::Message>(category: ProtocolCategory, protocol: &T) -> Result<Vec<u8>, SerializeError> {
+    let length = protocol.encoded_len();
+    if length > u16::max as usize {
+        return Err(SerializeError::BodyLengthExceeded);
+    }
+
+    let mut buf = Vec::with_capacity(HEADER_SIZE + length);
+    write_header(category, length as u16, &mut buf[..HEADER_SIZE].try_into().unwrap());
+    if let Err(e) = protocol.encode(&mut buf) {
+        return Err(SerializeError::EncodeError(e));
+    }
+
+    Ok(buf)
 }
 
 pub fn write_header(
     category: ProtocolCategory,
-    length: usize,
+    length: u16,
     buf: &mut [u8; HEADER_SIZE]
-) -> Result<(), SerializeError> {
+) {
     const RESERVED: u8 = 0u8;
-    
-    if length > u16::max as usize {
-        return Err(SerializeError::BodyLengthExceeded);
-    }
 
     buf[0] = category as u8;
     buf[1] = RESERVED;
     buf[2] = (length >> 8) as u8;
     buf[3] = length as u8;
-    
-    Ok(())
 }
 
 pub fn read_header(buf: &[u8; HEADER_SIZE]) -> (ProtocolCategory, u16) {
@@ -56,4 +68,33 @@ pub fn read_header(buf: &[u8; HEADER_SIZE]) -> (ProtocolCategory, u16) {
     let length =  ((buf[2] as u16) << 8) | (buf[3] as u16);
 
     (category, length)
+}
+
+pub mod convert {
+    use crate::*;
+    use nalgebra::{Point2, Vector2};
+
+    impl From<Vector2<f32>> for PVector2 {
+        fn from(value: Vector2<f32>) -> Self {
+            PVector2 { x: value.x, y: value.y }
+        }
+    }
+
+    impl From<PVector2> for Vector2<f32> {
+        fn from(value: PVector2) -> Self {
+            Vector2::new(value.x, value.y)
+        }
+    }
+
+    impl From<Point2<f32>> for PPoint2 {
+        fn from(value: Point2<f32>) -> Self {
+            PPoint2 { x: value.x, y: value.y }
+        }
+    }
+
+    impl From<PPoint2> for Point2<f32> {
+        fn from(value: PPoint2) -> Self {
+            Point2::new(value.x, value.y)
+        }
+    }
 }
