28c8b6375b23a4a514e1fce1327fc254c4c3594b
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-03-18 11:03:22 +0900

    Add util function

--- cs/Protocol.cs
+++ cs/Protocol.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Buffers;
+using Google.Protobuf;
 
 namespace spire.protocol;
 
@@ -15,12 +17,12 @@ public static class ProtocolHeader
     public const int HeaderSize = 4;
     private const byte Reserved = 0;
 
-    public static void Write(ProtocolCategory protocolCategory, ushort length, Span<byte> buffer)
+    public static void Write(ProtocolCategory category, ushort length, Span<byte> buffer)
     {
         if (buffer.Length < 4)
             throw new ArgumentOutOfRangeException("Buffer must be at least 4 bytes long.");
 
-        buffer[0] = (byte)protocolCategory;
+        buffer[0] = (byte)category;
         buffer[1] = Reserved;
         buffer[2] = (byte)(length >> 8);
         buffer[3] = (byte)length;
@@ -31,7 +33,7 @@ public static class ProtocolHeader
         if (buffer.Length < 4)
             throw new ArgumentOutOfRangeException("Buffer must be at least 4 bytes long.");
 
-        var protocol = buffer[0] switch
+        var category = buffer[0] switch
         {
             1 => ProtocolCategory.Auth,
             2 => ProtocolCategory.Game,
@@ -40,6 +42,22 @@ public static class ProtocolHeader
         };
         var length = (ushort)((buffer[2] << 8) | buffer[3]);
 
-        return (protocol, length);
+        return (category, length);
+    }
+}
+
+public static class ProtocolUtil
+{
+    public static byte[] Serialize(ProtocolCategory category, IMessage protocol)
+    {
+        var length = protocol.CalculateSize();
+        if (length > ushort.MaxValue)
+            throw new ArgumentOutOfRangeException($"Requested buffer length {length} exceed {ushort.MaxValue}.");
+        
+        var buffer = ArrayPool<byte>.Shared.Rent(ProtocolHeader.HeaderSize + length);
+        ProtocolHeader.Write(category, (ushort)length, buffer);
+        protocol.WriteTo(buffer.AsSpan()[ProtocolHeader.HeaderSize..]);
+
+        return buffer;
     }
 }
