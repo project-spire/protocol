d7dd3a4b97d86e1b3de0d120e7f9a99e6acbc0b0
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-04-06 17:28:42 +0900

    Add ProtocolHeader struct

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -25,50 +25,52 @@ pub enum ProtocolCategory {
     Net = 3,
 }
 
+pub struct ProtocolHeader {
+    pub category: ProtocolCategory,
+    pub length: usize,
+}
+
 #[derive(Debug, Clone)]
-pub enum SerializeError {
+pub enum SerializationError {
     BodyLengthExceeded,
     EncodeError(EncodeError),
 }
 
-pub fn serialize_protocol<T: prost::Message>(category: ProtocolCategory, protocol: &T) -> Result<Vec<u8>, SerializeError> {
+pub fn serialize_protocol<T: prost::Message>(category: ProtocolCategory, protocol: &T) -> Result<Vec<u8>, SerializationError> {
     let length = protocol.encoded_len();
     if length > u16::max as usize {
-        return Err(SerializeError::BodyLengthExceeded);
+        return Err(SerializationError::BodyLengthExceeded);
     }
 
+    let header = ProtocolHeader { category, length };
     let mut buf = Vec::with_capacity(HEADER_SIZE + length);
-    serialize_header(category, length as u16, &mut buf[..HEADER_SIZE].try_into().unwrap());
+    serialize_header(header, &mut buf[..HEADER_SIZE].try_into().unwrap());
     if let Err(e) = protocol.encode(&mut buf) {
-        return Err(SerializeError::EncodeError(e));
+        return Err(SerializationError::EncodeError(e));
     }
 
     Ok(buf)
 }
 
-pub fn serialize_header(
-    category: ProtocolCategory,
-    length: u16,
-    buf: &mut [u8; HEADER_SIZE]
-) {
+pub fn serialize_header(header: ProtocolHeader, buf: &mut [u8; HEADER_SIZE]) {
     const RESERVED: u8 = 0u8;
 
-    buf[0] = category as u8;
+    buf[0] = header.category as u8;
     buf[1] = RESERVED;
-    buf[2] = (length >> 8) as u8;
-    buf[3] = length as u8;
+    buf[2] = (header.length >> 8) as u8;
+    buf[3] = header.length as u8;
 }
 
-pub fn deserialize_header(buf: &[u8; HEADER_SIZE]) -> (ProtocolCategory, u16) {
+pub fn deserialize_header(buf: &[u8; HEADER_SIZE]) -> ProtocolHeader {
     let category = match buf[0] {
         1 => ProtocolCategory::Auth,
         2 => ProtocolCategory::Game,
         3 => ProtocolCategory::Net,
         _ => ProtocolCategory::None,
     };
-    let length =  ((buf[2] as u16) << 8) | (buf[3] as u16);
+    let length =  ((buf[2] as usize) << 8) | (buf[3] as usize);
 
-    (category, length)
+    ProtocolHeader { category, length }
 }
 
 pub mod convert {
