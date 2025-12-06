c49f96dcc78f33829b93ce85c8383e632c7e24af
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-12-06 20:59:01 +0900

    [rs] Extract library

--- lib/rs/generator/src/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("IO error: {0}")]
-    IO(#[from] std::io::Error),
-
-    #[error("JSON error: {0}")]
-    Json(#[from] serde_json::Error),
-}
--- lib/rs/src/lib.rs
+++
@@ -1,7 +0,0 @@
-include!(concat!(env!("OUT_DIR"), "/spire.protocol.rs"));
-
-pub mod game;
-
-pub mod lobby;
-
-pub mod convert;
